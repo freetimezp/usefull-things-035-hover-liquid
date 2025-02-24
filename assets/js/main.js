29fd7bee686bdc2e87bd7c985c23d3cfe5cfa177
freetimezp <kraftukrnet@ukr.net>
2025-02-24 19:58:26 +0200

    02. usefull-things-035-hover-liquid. UI. Hover Liquid animation.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector(".wrapper"),
+    intensity: 0.5,
+    image1: './assets/images/img_one.jpg',
+    image2: './assets/images/img_two.jpg',
+    displacementImage: './assets/images/transition.jpg'
+});
